11ed8ddc6588be86e850f09b90de63a374b1c521
razorhash <hashim.mazhar@gmail.com>
2020-05-05 20:13:38 +0200

    updated readme and keywords in setup.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # fmp
-A python wrapper for the Financial Model Prep API for analysis of public companies (Under Construction)
+A python wrapper for the Financial Model Prep API pro (fmpcloud.io) for analysis of public companies (Under Construction)
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/razorhash/fmp/",
     packages=setuptools.find_packages(),
-    keywords=["Financial Markets Prep", "python", "wrapper", "API"],
+    keywords=["Financial Markets Prep", "python", "wrapper", "API", "fmpcloud.io", "fmpcloud"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: MIT License",
