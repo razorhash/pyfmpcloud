074c740683ff357300b028488366eb64873e98c1
razorhash <hashim.mazhar@gmail.com>
2020-05-05 14:10:48 +0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fmp API wrapper", 
+    version="0.0.1",
+    author="razorhash",
+    author_email="hashim.mazhar@gmail.com",
+    description="A python-based wrapper for the Financial Markets Prep API for financial data of public companies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/razorhash/fmp/,
+    packages=setuptools.find_packages(),
+    keywords=["Financial Markets Prep", "python", "wrapper", "API"]
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+        "Target Audience :: Traders, Programmers, Finance and Risk Professionals",
+    ],
+    python_requires='>=3.6',
+)
