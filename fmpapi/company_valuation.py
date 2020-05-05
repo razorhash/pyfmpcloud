5850544d3b7fe58c3734b022fc8c50c9f9531991
razorhash <hashim.mazhar@gmail.com>
2020-05-05 18:45:48 +0200

    added income statement and cashflow statement. Also changed name bstype to ftype

--- fmpapi/company_valuation.py
+++ fmpapi/company_valuation.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen
 import pandas as pd
-#import error
+import json
 
 import configparser
 cfg = configparser.ConfigParser()
@@ -20,32 +20,86 @@ def rss_feed():
     data = response.read().decode("utf-8")
     return pd.read_json(data)
 
-def balance_sheet(ticker, period = 'annual', bstype = 'full'):
+def balance_sheet(ticker, period = 'annual', ftype = 'full'):
     """Balance sheet API from https://fmpcloud.io/documentation#balanceSheet
     
     Input:
         ticker : ticker for which we need the balance sheet values
         period : 'annual', 'quarter'. Periodicity of requested balance sheet. Defaults to annual
-        bstype : 'full', 'growth', 'short', 'growth-short'. Defines input sheet type. Defaults to full. 
+        ftype : 'full', 'growth', 'short', 'growth-short'. Defines input sheet type. Defaults to full. 
     Returns:
         Balance sheet info for selected tickers 
     """  
     
     typeurl = ''
     try:
-        if bstype == 'full':
+        if ftype == 'full':
             typeurl = 'balance-sheet-statement/'
-        elif bstype == 'growth':
+        elif ftype == 'growth':
             typeurl = 'balance-sheet-statement-growth/'
-        elif bstype == 'short':
+        elif ftype == 'short':
             typeurl = 'balance-sheet-statement-shorten/'
-        elif bstype == 'growth-short':
+        elif ftype == 'growth-short':
             typeurl = 'balance-sheet-statement-growth-shorten/'
     except:
-        print(error)
         print('Balance sheet type not correct')
         
     url = urlroot + typeurl + ticker + "?" + "datatype=csv&period=" + period + "&apikey=" + apikey
     data = pd.read_csv(url)
-#    data = response.read().decode("utf-8").text
     return data
+
+def income_statement(ticker, period = 'annual', ftype = 'full'):
+    """Income statement API from https://fmpcloud.io/documentation#incomeStatement
+    
+    Input:
+        ticker : ticker for which we need the balance sheet values
+        period : 'annual', 'quarter'. Periodicity of requested balance sheet. Defaults to annual
+        ftype : 'full', 'growth'. Defines input sheet type. Defaults to full. 
+    Returns:
+        Income statement info for selected tickers 
+    """
+    typeurl = ''
+    try:
+        if ftype == 'full':
+            typeurl = 'income-statement/'
+        elif ftype == 'growth':
+            typeurl = 'income-statement-growth/'
+#        elif bstype == 'short':
+#            typeurl = 'income-statement-shorten/'
+#        elif bstype == 'growth-short':
+#            typeurl = 'income-statement-growth-shorten/'
+    except:
+        print('Income statement type not correct')
+        
+    url = urlroot + typeurl + ticker + "?" + "period=" + period + "&apikey=" + apikey
+    response = urlopen(url)
+    data = response.read().decode("utf-8")
+    return pd.read_json(data)
+
+def cash_flow_statement(ticker, period = 'annual', ftype = 'full'):
+    """Cash Flow Statement API from https://fmpcloud.io/documentation#cashFlowStatement
+    
+    Input:
+        ticker : ticker for which we need the balance sheet values
+        period : 'annual', 'quarter'. Periodicity of requested balance sheet. Defaults to annual
+        ftype : 'full', 'growth'. Defines input sheet type. Defaults to full. 
+    Returns:
+        Income statement info for selected tickers 
+    """
+    typeurl = ''
+    try:
+        if ftype == 'full':
+            typeurl = 'cash-flow-statement/'
+        elif ftype == 'growth':
+            typeurl = 'cash-flow-statement-growth/'
+#        elif bstype == 'short':
+#            typeurl = 'income-statement-shorten/'
+#        elif bstype == 'growth-short':
+#            typeurl = 'income-statement-growth-shorten/'
+    except:
+        print('Cash Flow Statement type not correct')
+        
+    url = urlroot + typeurl + ticker + "?" + "period=" + period + "&apikey=" + apikey
+    response = urlopen(url)
+    data = response.read().decode("utf-8")
+    return pd.read_json(data)
