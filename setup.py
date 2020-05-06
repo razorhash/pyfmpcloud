dee477557a4dca1b1d761976636db91e061f5449
razorhash <hashim.mazhar@gmail.com>
2020-05-06 17:21:26 +0200

    update release number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyfmpcloud", 
-    version="0.1.2",
+    version="0.1.3",
     author="razorhash",
     author_email="hashim.mazhar@gmail.com",
     license="MIT",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/razorhash/fmp/",
-    download_url = "https://github.com/razorhash/fmp/archive/v0.1.2.tar.gz",
+    download_url = "https://github.com/razorhash/fmp/archive/v0.1.3.tar.gz",
     packages=setuptools.find_packages(),
     data_files=[('config',['pyfmpcloud/config.ini'])],
     keywords=["Financial Models Prep", "python", "wrapper", "API", "fmpcloud.io", "fmpcloud"],
