a4b209a2f35da9c0659d1b1c36999e6d6d580142
razorhash <hashim.mazhar@gmail.com>
2020-05-07 21:38:20 +0200

    added historical stock data function.

--- pyfmpcloud/stock_time_series.py
+++ pyfmpcloud/stock_time_series.py
@@ -42,10 +42,49 @@ def ticker_search(match = None, limit = 100, exchange = 'NASDAQ'):
     data = response.read().decode("utf-8")
     return pd.read_json(data)
 
-def historical_stock_data():
+def historical_stock_data(ticker, period = None, dailytype = None, last = None, start = None, end = None):
+    """Historical stock data API for partial matching of stocks over specified exchange. From https://fmpcloud.io/documentation#historicalStockData
+    
+    Input:
+        ticker - company for which you want the historical stock data
+        period - tick periodicity - can be '1min', '5min', '15min', '30min', '1hour'. Defaults to '15min'. Do not use with daily type
+        dailytype - can be 'line', 'change'. line chart info for daily stock or daily change and volume. Do not use with period.
+        last - stock data for last x days. Only works with dailytype. Does not work with period 
+        start - start date in the format yyyy-mm-dd. eg: '2018-01-01'
+        end - end date in the format yyyy-mm-dd. eg: '2019-01-01'
+    Returns:
+        Dataframe -- matching tickers, upto 'limit' number of values, found on the specified exchange
+    """
     urlroot = settings.get_urlroot()
     apikey = settings.get_apikey()
-    return 0
+    if ((dailytype is not None) or (last is not None)) and (period is not None):
+        raise Exception(" 'period' and 'dailytype' cannot be set on the same call. Please choose either, not both. 'last' can only be set with 'dailytype'")
+    if dailytype is not None:
+        urlhist = urlroot + 'historical-price-full/' + ticker + '?'
+    elif period is not None:
+        urlhist = urlroot + 'historical-chart/' + period + '/' + ticker + '?'
+    else:
+        raise Exception("'period' or 'dailytype' not set. Please set atleast one")
+    if dailytype == 'daily':
+        urlhist = urlhist + "serietype=line&"
+    if last is not None:
+        urlhist = urlhist + "timeseries=" + str(last) + "&"
+    if (last is None) and (start is not None):
+        urlhist = urlhist + "from=" + start + "?"
+    if (last is None) and (end is not None):
+        urlhist = urlhist + "to" + end + "?"
+    url = urlhist+ "apikey=" + apikey
+    response = urlopen(url)
+    data = response.read().decode("utf-8")
+    data = pd.read_json(data)
+    if dailytype is not None:
+        datatick = data['symbol']
+        data_mod = pd.DataFrame.from_records(data['historical'])
+        data_mod['symbol'] = datatick
+        data = data_mod
+    data['date'] = pd.to_datetime(data['date'], format = '%Y-%m-%d %H:%M:%S')
+    data = data.set_index('date')
+    return data
 
 def batch_request_eod_prices():
     urlroot = settings.get_urlroot()
